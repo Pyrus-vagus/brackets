59a5d9380b9f92ec582f915f7cc6cb26bb5e0093
pyrus-vagus <grushadaria@gmail.com>
2021-03-15 05:33:45 +0300

    feat:implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const brackets = bracketsConfig.reduce((a, b) => a.concat(b));
+    let stack = [];
+    for (let item of str) {
+        let brInd = brackets.indexOf(item);
+        let duplicate = brackets.filter(
+            (item, index) => brackets.indexOf(item) !== index
+        );
+        if (duplicate.indexOf(item) !== -1) {
+            if (stack.indexOf(item) === -1) {
+                stack.push(item);
+            } else if (stack.pop() !== item) {
+                return false;
+            }
+        } else if (brInd % 2 === 0) {
+            stack.push(brInd + 1);
+        } else {
+            if (stack.pop() !== brInd) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
